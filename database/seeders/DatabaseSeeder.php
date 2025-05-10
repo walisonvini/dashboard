265381382db8da747f6cf9b0283a0616d0317f51
walison.vitabe <walison.ribeiro@vitabe.com.br>
2025-05-10 19:07:49 -0300

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Database\Seeders\UserSeeder;
+use Database\Seeders\RolesPermissionsSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            UserSeeder::class,
+            RolesPermissionsSeeder::class,
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'super',
+            'email' => 'super@example.com',
+            'password' => Hash::make('password'),
+        ]);
+        
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'employee',
+            'email' => 'employee@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'user',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
