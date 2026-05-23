f265df130017048b75d9058bec09e7652482edc9
walisonvini <walison.vinicios12@gmail.com>
2026-05-23 10:58:26 -0300

    feat: simplify trustProxies middleware configuration for production environment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,17 +17,15 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
 
-        if (app()->environment('production')) {
-            $middleware->trustProxies(
-                at: '*',
-                headers:
-                    Request::HEADER_X_FORWARDED_FOR |
-                    Request::HEADER_X_FORWARDED_HOST |
-                    Request::HEADER_X_FORWARDED_PORT |
-                    Request::HEADER_X_FORWARDED_PROTO |
-                    Request::HEADER_X_FORWARDED_AWS_ELB
-            );
-        }
+        $middleware->trustProxies(
+            at: '*',
+            headers:
+                Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO |
+                Request::HEADER_X_FORWARDED_AWS_ELB
+        );
 
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
